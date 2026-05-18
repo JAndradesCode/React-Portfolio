e15b5cb89934adf7728b83bd73353c7db1bb107d
JAndradesCode <jandrades7188@gmail.com>
2026-05-18 05:40:18 -0700

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,122 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const About = () => {
+  const frontendSkills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "TypeScript",
+    "TailwindCSS",
+  ];
+
+  const backendSkills = ["Node.js", "MongoDB", "SQL"];
+
+  return (
+    <section
+      id="about"
+      className="min-h-screen flex items-center justify-center py-20 bg-neutral-950"
+    >
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-red-900 to-red-600 bg-clip-text text-transparent text-center">
+            {" "}
+            About Me
+          </h2>
+
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              Passionate developer eager to expand skillset and continue to improve skills with new projects.
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-red-500/10 text-red-500 py-1 px-3 rounded-full text-sm hover:bg-red-500/20
+                                    hover:shadow-[0_2px_8px_rgba(246, 61, 61, 0.28)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Backend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {backendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-red-500/10 text-red-500 py-1 px-3 rounded-full text-sm hover:bg-red-500/20
+                                    hover:shadow-[0_2px_8px_rgba(246, 61, 61, 0.28)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> Education </h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong>Graduated Sunrise Mountain High School</strong>{" "}
+                  (2022-2026)
+                </li>
+                <li>
+                  <strong>Graduated West-MEC</strong> - Coding Program
+                  (2024-2026)
+                </li>
+                <li>
+                  <strong> Attaining B.S. in Computer Science </strong> -
+                  Northern Arizona University (2026-2030)
+                </li>
+                <li>
+                  Relevant Coursework: Data Structures, Web Development,
+                  Database infrustructure...
+                </li>
+              </ul>
+            </div>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> Work Experience </h3>
+              <div className="space-y-4 text-gray-300">
+                <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    Intern at Sunrise Mountain High School - Peoria Student
+                    Broadcasting Network (2025 - 2026){" "}
+                  </h4>
+                  <p>
+                    Wrote and edited scripts and managed projects for daily
+                    broadcasting show.
+                  </p>
+                </div>
+
+                <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    Intern at Sunrise Mountain High School - Software
+                    Development (2025 - 2026){" "}
+                  </h4>
+                  <p>
+                    Explored utilities of JavaScript regarding video editing
+                    software and animation.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
